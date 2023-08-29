9cbc72c3e32e3ba0e3fba7c777973df4fb157ace
LenZuro <126126939+LenZuro@users.noreply.github.com>
2023-08-28 21:02:00 -0700

    its coming alive

---
+++ src/projects.jsx
@@ -0,0 +1,38 @@
+export const projects = [
+    {
+        projectTitle: "ViewFoliio",
+        description: "An app to keep track of stocks and index funds",
+        gitHub: "https://github.com/shadowfre4k/viewFolio",
+        liveLink: "https://secure-sands-25649-e4efe5e3aa6b.herokuapp.com/",
+    },
+    {
+        projectTitle: "First-portfoli0",
+        description: "My very first stab at creating a sleek and professinal portfolio",
+        gitHub: "https://github.com/LenZuro/First-portfolio",
+        liveLink: " https://lenzuro.github.io/First-portfolio/",
+    },
+    {
+        projectTitle: "Brewster",
+        description: "My first colabrative project where me and my team created a brewery social app to bring beer loves in the same commuinity toghther",
+        gitHub: "https://github.com/BradHamm/Brewster-Brew-Me-Bro",
+        liveLink: "https://bradhamm.github.io/Brewster-Brew-Me-Bro/",
+    },
+    {
+        projectTitle: "PWA Text Editor JEST ",
+        description: "It is a text editor that can be used offline so you can write and save your notes and code anywhere!",
+        gitHub: "https://github.com/LenZuro/PWA-Text-Editor",
+        liveLink: "",
+    },
+    {
+        projectTitle: "SQL Content Manager ",
+        description: "A full backend project where I made a buisness managment command line application",
+        gitHub: "https://github.com/LenZuro/SQL-content-Manager",
+        liveLink: "",
+    },
+    {
+        projectTitle: "NotBook",
+        description: "A social media API that ic created to share thoughts and reactions to those friends in youre network",
+        gitHub: "https://github.com/LenZuro/social-api",
+        liveLink: "",
+    },
+];
